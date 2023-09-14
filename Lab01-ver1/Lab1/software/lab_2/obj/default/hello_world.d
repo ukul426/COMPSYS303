99ed607e94416030cc2a5d26dad790f83093fb75
DulanyaW <dwit264@aucklanduni.ac.nz>
2023-09-14 14:25:41 +1200

    lab01

---
+++ Lab01-ver1/Lab1/software/lab_2/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c ../lab_2_bsp/system.h \
+ ../lab_2_bsp/linker.h ../lab_2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../lab_2_bsp//HAL/inc/io.h ../lab_2_bsp//HAL/inc/alt_types.h
+
+../lab_2_bsp/system.h:
+
+../lab_2_bsp/linker.h:
+
+../lab_2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../lab_2_bsp//HAL/inc/io.h:
+
+../lab_2_bsp//HAL/inc/alt_types.h:
