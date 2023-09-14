28434752cb64bca600e0b5a7824fb49b7c29fdf2
ukul426 <ukul426@aucklanduni.ac.nz>
2023-09-14 15:07:27 +1200

    Keypress excercise done

---
+++ Uma/303lab/DE2-115/software/Lab02Excer/obj/default/hello_world.d
@@ -0,0 +1,32 @@
+obj/default/hello_world.o: hello_world.c \
+ ../Lab02Excer_bsp//HAL/inc/sys/alt_alarm.h \
+ ../Lab02Excer_bsp//HAL/inc/sys/alt_llist.h \
+ ../Lab02Excer_bsp//HAL/inc/alt_types.h \
+ ../Lab02Excer_bsp//HAL/inc/priv/alt_alarm.h \
+ ../Lab02Excer_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Lab02Excer_bsp//HAL/inc/io.h ../Lab02Excer_bsp//HAL/inc/alt_types.h \
+ ../Lab02Excer_bsp/system.h ../Lab02Excer_bsp/linker.h \
+ ../Lab02Excer_bsp//HAL/inc/sys/alt_irq.h \
+ ../Lab02Excer_bsp//HAL/inc/nios2.h
+
+../Lab02Excer_bsp//HAL/inc/sys/alt_alarm.h:
+
+../Lab02Excer_bsp//HAL/inc/sys/alt_llist.h:
+
+../Lab02Excer_bsp//HAL/inc/alt_types.h:
+
+../Lab02Excer_bsp//HAL/inc/priv/alt_alarm.h:
+
+../Lab02Excer_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../Lab02Excer_bsp//HAL/inc/io.h:
+
+../Lab02Excer_bsp//HAL/inc/alt_types.h:
+
+../Lab02Excer_bsp/system.h:
+
+../Lab02Excer_bsp/linker.h:
+
+../Lab02Excer_bsp//HAL/inc/sys/alt_irq.h:
+
+../Lab02Excer_bsp//HAL/inc/nios2.h:
