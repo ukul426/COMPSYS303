f2a71443cb4afc34b2cece14c11f9ea9e8fa6025
ukul426 <ukul426@aucklanduni.ac.nz>
2023-10-12 15:39:14 +1300

    303 project buttons

---
+++ Uma/Project/DE2-115/DE2-115/software/cs303/obj/default/pacemaker.d
@@ -0,0 +1,5 @@
+obj/default/pacemaker.o: pacemaker.c timing.h pacemaker.h
+
+timing.h:
+
+pacemaker.h:
---
+++ Uma/Project/DE2-115/DE2-115/software/project/obj/default/hello_world.d
@@ -0,0 +1,35 @@
+obj/default/hello_world.o: hello_world.c ../project_bsp/system.h \
+ ../project_bsp/linker.h ../project_bsp//HAL/inc/sys/alt_alarm.h \
+ ../project_bsp//HAL/inc/sys/alt_llist.h \
+ ../project_bsp//HAL/inc/alt_types.h \
+ ../project_bsp//HAL/inc/priv/alt_alarm.h \
+ ../project_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../project_bsp//HAL/inc/io.h ../project_bsp//HAL/inc/alt_types.h \
+ ../project_bsp//HAL/inc/sys/alt_irq.h ../project_bsp//HAL/inc/nios2.h \
+ pacemaker.h timing.h
+
+../project_bsp/system.h:
+
+../project_bsp/linker.h:
+
+../project_bsp//HAL/inc/sys/alt_alarm.h:
+
+../project_bsp//HAL/inc/sys/alt_llist.h:
+
+../project_bsp//HAL/inc/alt_types.h:
+
+../project_bsp//HAL/inc/priv/alt_alarm.h:
+
+../project_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../project_bsp//HAL/inc/io.h:
+
+../project_bsp//HAL/inc/alt_types.h:
+
+../project_bsp//HAL/inc/sys/alt_irq.h:
+
+../project_bsp//HAL/inc/nios2.h:
+
+pacemaker.h:
+
+timing.h:
