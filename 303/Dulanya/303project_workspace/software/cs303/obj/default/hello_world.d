fadc22383c1cc6dab3fd1450d68fcd27c42adffa
DulanyaW <dwit264@aucklanduni.ac.nz>
2023-10-16 15:09:30 +1300

    uart part update

---
+++ 303/Dulanya/303project_workspace/software/cs303/obj/default/hello_world.d
@@ -0,0 +1,34 @@
+obj/default/hello_world.o: hello_world.c ../cs303_bsp/system.h \
+ ../cs303_bsp/linker.h ../cs303_bsp//HAL/inc/sys/alt_alarm.h \
+ ../cs303_bsp//HAL/inc/sys/alt_llist.h ../cs303_bsp//HAL/inc/alt_types.h \
+ ../cs303_bsp//HAL/inc/priv/alt_alarm.h \
+ ../cs303_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../cs303_bsp//HAL/inc/io.h ../cs303_bsp//HAL/inc/alt_types.h \
+ ../cs303_bsp//HAL/inc/sys/alt_irq.h ../cs303_bsp//HAL/inc/nios2.h \
+ pacemaker.h timing.h
+
+../cs303_bsp/system.h:
+
+../cs303_bsp/linker.h:
+
+../cs303_bsp//HAL/inc/sys/alt_alarm.h:
+
+../cs303_bsp//HAL/inc/sys/alt_llist.h:
+
+../cs303_bsp//HAL/inc/alt_types.h:
+
+../cs303_bsp//HAL/inc/priv/alt_alarm.h:
+
+../cs303_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../cs303_bsp//HAL/inc/io.h:
+
+../cs303_bsp//HAL/inc/alt_types.h:
+
+../cs303_bsp//HAL/inc/sys/alt_irq.h:
+
+../cs303_bsp//HAL/inc/nios2.h:
+
+pacemaker.h:
+
+timing.h:
